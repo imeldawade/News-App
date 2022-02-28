41b06d1147e8feb887a9886b871ad641aba40154
imeldawade <imelda.wade@student.moringaschool.com>
2022-02-28 11:10:40 +0300

    initialized application

---
+++ init .py
@@ -0,0 +1,12 @@
+from flask import Flask
+from .config import DevConfig
+
+app = Flask(__name__)
+
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+from app import news
+
+
+
