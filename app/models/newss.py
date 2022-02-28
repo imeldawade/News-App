1432a87d2308b41a5ac0ce776fe8ccb7d2c23602
imeldawade <imelda.wade@student.moringaschool.com>
2022-02-28 15:47:44 +0300

    define Newss class

--- app/models/newss.py
+++ app/models/newss.py
@@ -1,3 +1,12 @@
 class Newss:
 
-    def __init__(self,)
+    def __init__(self,urlToImage,title,description,url,time,content):
+        self.urlToImage=urlToImage
+        self.title=title
+        self.description=description
+        self.url=url
+        self.time=time
+        self.content=content
+
+        
+        
--- app/newss_test.py
+++ app/newss_test.py
@@ -2,7 +2,15 @@ import unittest
 from app.models import newss
 News = newss.News
 
-class NewsTest(unittest.TestCase)
+class NewsTest(unittest.TestCase):
 
     def setUp(self):
         self.new_News = News()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_newss,News))
+
+if __name__ == '__main__':
+    unittest.main()        
+
+         
