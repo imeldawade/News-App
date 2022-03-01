82e78f68bb7ea5b60ae59844f44e7d809c2f679b
imeldawade <imelda.wade@student.moringaschool.com>
2022-03-02 00:34:12 +0300

    add heading

--- app/views.py
+++ app/views.py
@@ -6,9 +6,11 @@ from .request import get_newss
 @app.route('/')
 def index():
 
+
     popular_newss = get_newss
-    print(popular_newss)
-    title = 'Home - Top Headlines'
+
+    # print(popular_newss)
+    title = 'Top Headlines'
     return render_template('index.html', title = title,popular = popular_newss)
 
-    
+
